f67bfab9e4b1bf1aec5189d04ead1b14fd1b6165
Nilotpal04 <nilotpalb2004@gmail.com>
2026-05-26 19:13:29 +0530

    add hardcoded photo gallery data

---
+++ src/data/photos.js
@@ -0,0 +1,35 @@
+const photos = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+    title: "Mountains",
+    location: "Iceland",
+  },
+
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1493246507139-91e8fad9978e",
+    title: "Forest",
+    location: "Norway",
+  },
+
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1514565131-fce0801e5785",
+    title: "City Lights",
+    location: "Tokyo",
+  },
+
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+    title: "Desert",
+    location: "Morocco",
+  },
+]
+
+export default photos
